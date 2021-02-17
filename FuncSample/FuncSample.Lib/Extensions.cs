aca91f8fd0d7dcd583d00da2ee680d85cd45d6b7
erionpc <erionpc@yahoo.it>
2021-02-17 00:17:32 +0000

    missed files

---
+++ FuncSample/FuncSample.Lib/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncSample.Lib
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> GetCombinations<T>(this IEnumerable<T> source)
+        {
+            if (null == source)
+                throw new ArgumentNullException(nameof(source));
+
+            T[] data = source.ToArray();
+
+            return Enumerable
+                .Range(0, 1 << (data.Length))
+                .Select(index => data
+                    .Where((v, i) => (index & (1 << i)) != 0)
+                .ToArray())
+                .Where(x => x.Any());
+        }
+    }
+}
---
+++ FuncSample/FuncSample.Lib/SqlQueryHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncSample.Lib
+{
+    public class SqlQueryHelper
+    {
+        public static string AppendOrBlockToQuery(string query, Dictionary<string, bool> orFilters)
+        {
+            if (!orFilters?.Any() ?? true)
+                return query;
+
+            if (string.IsNullOrWhiteSpace(query))
+                return string.Empty;
+
+            var criteria = orFilters.Select(x => Tuple.Create<Func<bool>, string>(() => x.Value, $"{x.Key} = 1"));
+
+            return $"{query} AND ({GetOrBlock(criteria)})";
+        }
+
+        private static string GetOrBlock(IEnumerable<Tuple<Func<bool>, string>> criteria)
+        {
+            string orBlock = "";
+
+            var filterCombinations = criteria.GetCombinations().OrderByDescending(x => x.Count());
+
+            foreach (var filterCriteria in filterCombinations)
+            {
+                if (filterCriteria.Select(x => x.Item1).All(x => x()))
+                {
+                    orBlock += string.Join(" OR ", filterCriteria.Select(x => x.Item2));
+                    break;
+                }
+            }
+
+            return orBlock;
+        }
+    }
+}
